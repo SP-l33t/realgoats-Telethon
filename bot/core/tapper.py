07fdcbf9e759e7111d6800396afb0feeba0717fd
Slayer <slayerpro27@gmail.com>
2024-09-30 22:42:03 +0300

    Added a setting to disable automatic checking and replacement of non-working proxies before startup

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -167,7 +167,10 @@ class Tapper:
                                        url=f'https://api-checkin.goatsbot.xyz/checkin/action/{checkin_id}')
 
     async def check_proxy(self, http_client: aiohttp.ClientSession) -> bool:
-        proxy_conn = http_client._connector
+        proxy_conn = http_client.connector
+        if proxy_conn and not hasattr(proxy_conn, '_proxy_host'):
+            logger.info(self.log_message(f"Running Proxy-less"))
+            return True
         try:
             response = await http_client.get(url='https://ifconfig.me/ip', timeout=aiohttp.ClientTimeout(15))
             logger.info(self.log_message(f"Proxy IP: {await response.text()}"))
